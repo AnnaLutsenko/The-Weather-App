b279334d6de69f5cc93f8e95ed2d3bbd8b4e21d7
Anna <anna@Mac-mini-Anna.local>
2016-05-14 21:40:26 +0300

    sprint 1

---
+++ Weather/Weather/ALTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  ALTableViewController.h
+//  Weather
+//
+//  Created by Anna on 14.05.16.
+//  Copyright © 2016 Anna Lutsenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ALTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray* cities;
+
+@end
--- Weather/Weather/AppDelegate.m
+++ Weather/Weather/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "ALTableViewController.h"
 
 @interface AppDelegate ()
 
