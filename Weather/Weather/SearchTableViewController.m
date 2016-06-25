8ac8bb1a21f670ea30e2ca4d6d97d77503cb3a31
Anna <anna@Mac-mini-Anna.local>
2016-06-25 22:51:01 +0300

    added search

--- Weather/Weather/SearchTableViewController.m
+++ Weather/Weather/SearchTableViewController.m
@@ -14,22 +14,90 @@
 
 @implementation SearchTableViewController
 
+- (NSMutableArray *)filteredContentList {
+    
+    if (!_filteredContentList) {
+        _filteredContentList = [NSMutableArray new];
+    }
+    
+    return _filteredContentList;
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    NSDictionary *Vyshkovo = @{@"_id":@688717,@"name":@"Vyshkovo",@"country":@"UA"};
+    NSDictionary *Vylok = @{@"_id":@688749,@"name":@"Vylok",@"country":@"UA"};
+    NSDictionary *Vylkove = @{@"_id":@688750,@"name":@"Vylkove",@"country":@"UA"};
+    NSDictionary *Vvedenka = @{@"_id":@688805,@"name":@"Vvedenka",@"country":@"UA"};
+    NSDictionary *Vradiyivka = @{@"_id":@688846,@"name":@"Vradiyivka",@"country":@"UA"};
+    NSDictionary *Voznesenka = @{@"_id":@688865,@"name":@"Voznesenka",@"country":@"UA"};
+    NSDictionary *Voykove = @{@"_id":@688904,@"name":@"Voykove",@"country":@"UA"};
+    NSDictionary *Voskhod = @{@"_id":@688951,@"name":@"Voskhod",@"country":@"UA"};
+    NSDictionary *Vorozhba = @{@"_id":@688961,@"name":@"Vorozhba",@"country":@"UA"};
+    
+    
+    self.cities = [NSMutableArray arrayWithObjects:Vylok, Vylkove, Vvedenka, Vyshkovo, Vradiyivka, Voznesenka, Voykove, Voskhod, Vorozhba, nil];
     
     
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+}
+#pragma mark - Search from Dictionary
+// firstSection is array which already filled.
+// contentList array for value of particular key
+// filteredContentList is search array from actual array.
+
+- (void)searchTableList {
+    
+    //Remove all objects first.
+    [self.filteredContentList removeAllObjects];
+    
+    NSString *searchString = self.citySearchBar.text;
+    
+    NSPredicate *filterPredicate = [NSPredicate predicateWithFormat:@"name beginswith[c] %@", searchString];
+    NSArray *filteredArr = [self.cities filteredArrayUsingPredicate:filterPredicate];
+    if(self.contentList.count > 0) {
+        [self.contentList removeAllObjects];
+    }
+    [self.filteredContentList addObjectsFromArray:filteredArr];
+    
+    [self.tableView reloadData];
 }
 
-- (NSMutableArray *)cities {
-    if (!_cities) {
-        _cities = [NSMutableArray new];
+- (void)searchBarTextDidBeginEditing:(UISearchBar *)searchBar {
+    
+    if ([searchBar.text length] != 0) {
+        self.isSearching = YES;
     }
-return _cities;
+    else {
+        self.isSearching = NO;
+    }
+}
+
+- (void)searchBar:(UISearchBar *)searchBar textDidChange:(NSString *)searchText {
+    NSLog(@"Text change - %d", self.isSearching);
+    
+    if([searchText length] != 0) {
+        self.isSearching = YES;
+        [self searchTableList];
+    }
+    else {
+        self.isSearching = NO;
+    }
+}
+
+- (void)searchBarCancelButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"Cancel clicked");
+}
+
+- (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
+    NSLog(@"Search Clicked");
+    [self searchTableList];
+    [searchBar resignFirstResponder];
 }
 
 #pragma mark - Action
@@ -40,33 +108,10 @@ return _cities;
     
 }
 
-#pragma mark - UITableViewDataSource
-
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    
-    return [self.cities count];
-}
-
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    
-    static NSString* identifier = @"Cell";
-    
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:identifier];
-    
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:identifier];
-    }
-    
-    cell.textLabel.text = [NSString stringWithFormat:@"%@", [self.cities objectAtIndex:indexPath.row]];
-    
-    return cell;
-}
-
 
 
 #pragma mark - UISearchBarDelegate
-
+/*
 - (void)searchBarSearchButtonClicked:(UISearchBar *)searchBar {
     
     [self.cities removeAllObjects];
@@ -85,16 +130,41 @@ return _cities;
     searchBar.text = @"";
     
 }
+*/
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    
+    return (self.filteredContentList != nil) ? [self.filteredContentList count] : 0;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString* cellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellIdentifier forIndexPath:indexPath];
+    
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellIdentifier];
+    }
+    
+    cell.textLabel.text = [NSString stringWithFormat:@"%@", self.filteredContentList[indexPath.row][@"name"]];
+    
+    return cell;
+}
 
 #pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     
-    NSString* selectedCity = [self.cities objectAtIndex:indexPath.row];
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
     
-    [self.delegate citySelected:selectedCity];
+//    NSString* selectedCity = [self.cities objectAtIndex:indexPath.row];
+//    
+//    [self.delegate citySelected:selectedCity];
     
-    [self dismissViewControllerAnimated:YES completion:^{}];
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 
